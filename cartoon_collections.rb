374651410cc590cfd8ec199ecac1c4f1b5d15b80
melia miller <meliajm@gmail.com>
2019-11-29 23:02:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,12 @@ def find_the_cheese(array) # code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   i = 0 
   while i < cheese_types.length do 
-    cheese_types
+    if array.include?(cheese_types[i])
+      cheese_types[i]
+    else
+      nil
+    end
   i += 1 
   end
-  array.include?(cheese_types.each_with_index)
+  
 end
