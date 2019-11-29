fd32f9763517fbbcbcf8bc076b9f2bba3dbd9472
melia miller <meliajm@gmail.com>
2019-11-29 22:49:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 def summon_captain_planet(array) # code an argument here
   # Your code here
   array.map() { |word, a| a << "#{word.capitalize}!"}
-  
+  array.collect
 end
 
 def long_planeteer_calls(array) # code an argument here
