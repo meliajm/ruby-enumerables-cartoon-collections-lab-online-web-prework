3068bb1ff87ecadc4ba3b722af22890bb931f257
melia miller <meliajm@gmail.com>
2019-11-29 22:35:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(array) # code an argument here
   # Your code here
-  array.map {}
+  array.map { |word| word.Capitalize}
 end
 
 def long_planeteer_calls(array) # code an argument here
