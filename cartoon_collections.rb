8946abd7d2c117bc862d95ebe0e426105e5eeb4c
melia miller <meliajm@gmail.com>
2019-11-29 22:45:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,10 @@
 require 'pry'
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  # array.each_with_index { |name, i| "puts #{i+1}. #{name}"}
+  array.each_with_index { |name, i| puts "#{i+1}. #{name}"}
   # binding.pry
   # array.each_with_index { |name| "puts #{name}"
-  array.each_with_index { |name| puts name }
+  # array.each_with_index { |name| puts name }
 end
 
 def summon_captain_planet(array) # code an argument here
