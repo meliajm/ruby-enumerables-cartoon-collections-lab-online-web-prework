6afa74ec91f51abe50eb82ea1e9f083e8817ad01
melia miller <meliajm@gmail.com>
2019-11-29 22:33:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,11 +2,7 @@ require 'pry'
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index { |name, i| puts "#{i+1}. #{name}" }
-  # i = 0 
-  # while i < array.length do 
-  #   puts array[i]
-  # i += 1 
-  # end
+  
 end
 
 def summon_captain_planet# code an argument here
