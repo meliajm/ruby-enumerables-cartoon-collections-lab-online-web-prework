261b1f08bee2004f2f866a144b34f5f31fa8bd1e
melia miller <meliajm@gmail.com>
2019-11-29 22:50:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,14 +2,11 @@ require 'pry'
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index { |name, i| puts "#{i+1}. #{name}"}
-  # binding.pry
-  # array.each_with_index { |name| "puts #{name}"
-  # array.each_with_index { |name| puts name }
+  
 end
 
 def summon_captain_planet(array) # code an argument here
-  # Your code here
-  # array.map { |word, a| a << "#{word.capitalize}!"}
+  
   array.collect { |word| word.capitalize + "!" }
 end
 
