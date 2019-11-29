af880aa0ea818bb926408e590d6d104b59defcda
melia miller <meliajm@gmail.com>
2019-11-29 22:27:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ require 'pry'
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index { |name| puts name }
-  # binding.pry
+  
 end
 
 def summon_captain_planet# code an argument here
